81aaa0d42f79be3edfb095132c92a92df643e3ae
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-04-28 21:01:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,17 @@
 def oxford_comma(array)
-  last = array.last
-  array.pop
-  new_string = array.join(", ")
-  new_string << ", and #{last}"
+  
   
   if array.size <= 1
     
   elsif array.size == 2
-  
-else
-    
+    last = array.last
+    array.pop
+    new_string = array.join(", ")
+    new_string << " and #{last}"
+  else
+    last = array.last
+    array.pop
+    new_string = array.join(", ")
+    new_string << ", and #{last}"
   end 
 end
